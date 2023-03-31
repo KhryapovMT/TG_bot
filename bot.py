89a7962e4d826075a4fc710b5bf7a5d46d92a260
KhryapovMT <36191973+KhryapovMT@users.noreply.github.com>
2023-03-31 16:15:35 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import os
 import logging
-from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
+from telegram import Updater, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import CommandHandler, CallbackQueryHandler, MessageHandler
 import requests
 import json
