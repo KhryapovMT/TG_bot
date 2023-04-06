c697fbeb971a2a6c2c3b0fe7d5d495f3811fb723
KhryapovMT <36191973+KhryapovMT@users.noreply.github.com>
2023-04-06 11:13:32 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,11 +2,12 @@ import os
 import random
 import logging
 from collections import defaultdict
-from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
-from telegram.ext import Updater, CommandHandler, CallbackContext, CallbackQueryHandler, MessageHandler, Filters
 import requests
 import json
 import datetime
+
+from telegram.ext import Update, InlineKeyboardMarkup, InlineKeyboardButton
+from telegram.ext import Updater, CommandHandler, CallbackContext, CallbackQueryHandler, MessageHandler, Filters
 
 # Set up the logging
 logging.basicConfig(level=logging.INFO,
@@ -37,188 +38,139 @@ BADGES = {
 
 updater = Updater(API_TOKEN, use_context=True)
 
-dispatcher.add_handler(CommandHandler("start", start))
-dispatcher.add_handler(CommandHandler("help", help_command))
+dispatcher = updater.dispatcher
 
-def start(update: Update, context: CallbackContext):
-    user = update.effective_user
-    context.user_data.setdefault('footprint', 0)
-    context.user_data.setdefault('history', [])
-    
-    text = f"Hello {user.first_name}! Welcome to the Carbon Footprint Calculator bot. 🌍\n"
-    text += "To calculate your carbon footprint, please select your transportation mode below:\n"
-    
+def transportation_mode_callback(update: Update, context: CallbackContext):
+    query = update.callback_query
+    query.answer()
+
+    mode = query.data
+    context.user_data['mode'] = mode
+
+    text = f"You have chosen {mode.capitalize()}. Please enter the distance you traveled in kilometers (e.g. 10)."
+    query.edit_message_text(text)
+
+    return "GET_DISTANCE"
+
+
+def get_distance_message(update: Update, context: CallbackContext):
+    try:
+        distance = float(update.message.text)
+    except ValueError:
+        update.message.reply_text("Invalid input. Please provide a numeric value for the distance.")
+        return "GET_DISTANCE"
+
+    mode = context.user_data['mode']
+    emission_factor = EMISSION_FACTORS[mode]
+    emission = emission_factor * distance
+
+    context.user_data['footprint'] = context.user_data.get('footprint', 0) + emission
+
+    today = datetime.date.today().isoformat()
+    history_entry = {'date': today, 'mode': mode, 'distance': distance, 'emission': emission}
+    context.user_data['history'].append(history_entry)
+
+    store_transportation_data(update, context)
+
+    text = f"You have traveled {distance} km using {mode.capitalize()}, emitting {emission:.2f} kg CO2e.\n\n"
+    text += format_statistics(context.user_data)
+    text += "\n\nWould you like to calculate another mode of transportation or view your statistics?"
+    keyboard = [        [InlineKeyboardButton("Calculate Another", callback_data="another"),         InlineKeyboardButton("View Statistics", callback_data="statistics")]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    update.message.reply_text(text, reply_markup=reply_markup)
+
+    return "CHOOSE_ACTION"
+
+
+def another_callback(update: Update, context: CallbackContext):
+    query = update.callback_query
+    query.answer()
+
+    text = "Please select another mode of transportation:"
     keyboard = [
         [InlineKeyboardButton("Car 🚗", callback_data="car"),
          InlineKeyboardButton("Public Transport 🚌", callback_data="public_transport"),
          InlineKeyboardButton("Bicycle 🚲", callback_data="bicycle"),
          InlineKeyboardButton("Walking 🚶‍♂️", callback_data="walking")]
     ]
-    
     reply_markup = InlineKeyboardMarkup(keyboard)
-    update.message.reply_text(text, reply_markup=reply_markup)
+    query.edit_message_text(text, reply_markup=reply_markup)
 
+    return "CHOOSE_TRANSPORTATION_MODE"
 
-def help_command(update: Update, context: CallbackContext):
-    help_text = "To use this bot, simply select your transportation mode and follow the instructions provided."
-    help_text += " You can also use the following commands:\n"
-    help_text += "/start - Start the bot and select transportation mode.\n"
-    help_text += "/help - Show help information.\n"
-    help_text += "/statistics - Show your carbon footprint statistics.\n"
-    help_text += "/setgoal - Set a goal for carbon footprint reduction (e.g. /setgoal 100).\n"
-    help_text += "/suggestchallenge - Get challenges to help you reach your goal.\n"
-    help_text += "/achievements - Show your badges and achievements.\n"
-    help_text += "/share - Share your achievements with others.\n"
-    help
+def update_achievements(user_data):
+    achievements = user_data.get("achievements", [])
 
-dispatcher.add_handler(CommandHandler("start", start))
-dispatcher.add_handler(CommandHandler("help", help_command))
-
-def format_statistics(user_data, time_period=None):
-    if time_period is not None:
-        start_date = datetime.date.today() - datetime.timedelta(days=time_period)
-        history = [entry for entry in user_data['history'] if datetime.date.fromisoformat(entry['date']) >= start_date]
-    else:
-        history = user_data['history']
-
-    total_emission = sum(entry['emission'] for entry in history)
-    mode_emissions = {mode: 0 for mode in EMISSION_FACTORS.keys()}
-    for entry in history:
-        mode_emissions[entry['mode']] += entry['emission']
+    total_emission = user_data.get('footprint', 0)
+    new_achievements = []
+    for badge_id, badge in BADGES.items():
+        if badge_id in achievements:
+            continue
+        if total_emission >= badge['threshold']:
+            new_achievements.append(badge_id)
     
-    text = "Carbon Footprint Statistics:\n"
-    text += f"{'Mode':<17}{'Emission (kg CO2e)'}\n"
-    for mode, emission in mode_emissions.items():
-        text += f"{mode.capitalize():<17}{emission:.2f}\n"
-    text += f"{'Total':<17}{total_emission:.2f}"
-    return text
+    if new_achievements:
+        user_data['achievements'] = achievements + new_achievements
+        text = "🎉 Congratulations on earning new achievements!\n\n"
+        for badge_id in new_achievements:
+            badge = BADGES[badge_id]
+            text += f"{badge['name']} - {badge['description']}\n"
+        return text
 
-
-def statistics(update: Update, context: CallbackContext):
-    time_period = None
-    if 'period' in context.user_data:
-        time_period = context.user_data['period']
-    if 'goal' in context.user_data:
-        goal = context.user_data['goal']
-        remaining = goal - total_emission
-        text += f"\n\nYour goal is to reduce {goal} kg CO2e. You have {remaining:.2f} kg CO2e remaining to reach your goal."
-  
-    text = format_statistics(context.user_data, time_period)
-    update.message.reply_text(text)
-
-dispatcher.add_handler(CommandHandler("statistics", statistics))
+    return None
 
 def store_transportation_data(update: Update, context: CallbackContext):
+    mode = context.user_data.get('mode')
+    if mode is None:
+        update.message.reply_text("Error: Mode not found in user data.")
+        return
+
+    emission_factor = EMISSION_FACTORS.get(mode)
+    if emission_factor is None:
+        update.message.reply_text("Error: Emission factor not found for mode.")
+        return
+
+    history_entry = context.user_data['history'][-1]
+    distance = history_entry['distance']
+
+
+    emission = emission_factor * distance
+    context.user_data['footprint'] = context.user_data.get('footprint', 0) + emission
+    context.user_data['history'].append({
+        "date": datetime.date.today().isoformat(),
+        "mode": mode,
+        "distance": distance,
+        "emission": emission
+    })
+
     badge_message = update_achievements(context.user_data)
-    
+
     if badge_message:
         update.message.reply_text(badge_message)
-        
+
     points_earned = update_points(context.user_data, mode)
     update.message.reply_text(f"You have earned {points_earned} points for choosing {mode.capitalize()}!")
-
-def achievements(update: Update, context: CallbackContext):
-    achievements = context.user_data.get("achievements", [])
-    if not achievements:
-        update.message.reply_text("You haven't earned any badges yet. Keep reducing your carbon footprint to unlock achievements!")
-        return
-
-    text = "🏆 Your Achievements:\n\n"
-    for badge_id in achievements:
-        badge = BADGES[badge_id]
-        text += f"{badge['name']} - {badge['description']}\n"
-    
-    update.message.reply_text(text)
-
-dispatcher.add_handler(CommandHandler("achievements", achievements))
-
+	
 def update_points(user_data, mode):
-    points_earned = POINTS.get(mode, 0)
-    user_data['points'] = user_data.get('points', 0) + points_earned
-    return points_earned
+    points = POINTS.get(mode, 0)
+    user_data['points'] = user_data.get('points', 0) + points
+    return points
 
-def set_goal(update: Update, context: CallbackContext):
-    if not context.args:
-        update.message.reply_text("Please provide a goal in kg CO2e. Example: /setgoal 100")
-        return
-
-    try:
-        goal = float(context.args[0])
-    except ValueError:
-        update.message.reply_text("Invalid input. Please provide a numeric value for the goal.")
-        return
-
-    context.user_data['goal'] = goal
-    update.message.reply_text(f"Your goal is set to reduce {goal} kg CO2e. Keep choosing eco-friendly transportation modes to achieve your goal!")
-
-dispatcher.add_handler(CommandHandler("setgoal", set_goal, pass_args=True))
-
-def suggest_challenge(update: Update, context: CallbackContext):
-    if 'goal' not in context.user_data:
-        update.message.reply_text("Please set a goal first using /setgoal command.")
-        return
-
-    goal = context.user_data['goal']
-    remaining = goal - context.user_data.get('footprint', 0)
-
-    if remaining <= 0:
-        update.message.reply_text("Congratulations! You've already achieved your goal. Set a new goal with /setgoal command.")
-        return
-
-    challenges = [
-        f"Use public transport instead of driving for {remaining / (EMISSION_FACTORS['car'] - EMISSION_FACTORS['public_transport']):.1f} km.",
-        f"Ride a bicycle instead of driving for {remaining / (EMISSION_FACTORS['car'] - EMISSION_FACTORS['bicycle']):.1f} km.",
-        f"Walk instead of driving for {remaining / (EMISSION_FACTORS['car'] - EMISSION_FACTORS['walking']):.1f} km.",
-    ]
-
-    text = "Here are some challenges to help you reach your goal:\n"
-    for challenge in challenges:
-        text += f"- {challenge}\n"
-
-    update.message.reply_text(text)
-
-dispatcher.add_handler(CommandHandler("suggestchallenge", suggest_challenge))
-
-
-def share(update: Update, context: CallbackContext):
-    user = update.effective_user
-    achievements = context.user_data.get("achievements", [])
-    if not achievements:
-        update.message.reply_text("You don't have any achievements to share yet.")
-        return
-
-    text = f"{user.first_name} has been using the Carbon Footprint Calculator bot and has earned the following achievements:\n\n"
-    for badge_id in achievements:
-        badge = BADGES[badge_id]
-        text += f"{badge['name']} - {badge['description']}\n"
-    
-    text += "\nJoin the carbon footprint reduction journey using the Carbon Footprint Calculator bot: https://t.me/Carbon_calc_bot"
-    update.message.reply_text(text)
-
-dispatcher.add_handler(CommandHandler("share", share))
-    
-def reset(update: Update, context: CallbackContext):
-    context.user_data['footprint'] = 0
-    context.user_data['history'] = []
-    update.message.reply_text("Your carbon footprint data has been reset.")
-
-dispatcher.add_handler(CommandHandler("reset", reset))
 
 def main():
     # Start the bot
     updater.start_polling()
-    print("Carbon Footprint Calculator bot is running...")
+    logger.info("Carbon Footprint Calculator bot is running...")
 
     # Run the bot until the user presses Ctrl+C or the process receives SIGINT, SIGTERM, or SIGABRT
     updater.idle()
-    print("Carbon Footprint Calculator bot has been stopped.")
+    logger.info("Carbon Footprint Calculator bot has been stopped.")
 
 
 if __name__ == '__main__':
-    # Start the bot
-    updater.start_polling()
-
-    # Run the bot until the user presses Ctrl+C or the process receives SIGINT,
-    # SIGTERM, or SIGABRT
-    updater.idle()
-
+    try:
+        main()
+    except KeyboardInterrupt:
+        updater.stop()
+        logger.info("\nCarbon Footprint Calculator bot has been stopped by user.")
